3045a0ee892f99fa2d1137899b3da71fff44f252
Mustafa Ozan Alpay <frozsgy@gmail.com>
2018-11-29 22:27:05 +0300

    init

---
+++ 219.php
@@ -0,0 +1,128 @@
+<?php
+#include("1192.php");
+$upsveritabani='Veritabani hatasi';
+$DBHost = 'localhost';
+$DBUser = 'root';
+$DBPass = '';
+$DBName = 'metu_219';
+$link=mysqli_connect($DBHost, $DBUser, $DBPass, $DBName);
+function httpPost($url,$params)
+{
+  $postData = '';
+   foreach($params as $k => $v)
+   {
+      $postData .= $k . '='.$v.'&';
+   }
+   $postData = rtrim($postData, '&');
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, count($postData));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    $output=curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+function tr_strtolower($text)
+{
+    $search=array("Ç","Ý","Ðž","Ö","Þž","Ü");
+    $replace=array("C","I","G","O","S","U");
+    $text=str_replace($search,$replace,$text);
+    return $text;
+}
+function getData($id) {
+  global $link;
+  $params = array(
+    "id" => $id,
+    "Submit" => "Show Info");
+  $return=httpPost("http://ma219.math.metu.edu.tr/SIS/sis.php",$params);
+
+preg_match_all('/<tr><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td>/msi', $return, $output);
+
+  $id=@$output[1][1];
+  $ln=@$output[2][1];
+  $ln=tr_strtolower($ln);
+  $fn=@$output[3][1];
+  $fn=tr_strtolower($fn);
+
+  $mt1=@$output[11][1];
+
+  $queery="INSERT INTO `2018` VALUES ('$id', '$ln', '$fn', '$mt1');";
+  mysqli_query($link, $queery);
+
+  if(strlen($fn) > 2) {
+  print "$id - $fn $ln imported\n";
+  }
+
+
+
+
+}
+
+//getData(2309615);
+
+function Luhn($number, $iterations = 1)
+{
+    while ($iterations-- >= 1)
+    {
+        $stack = 0;
+        $number = str_split(strrev($number), 1);
+        foreach ($number as $key => $value)
+        {
+            if ($key % 2 == 0)
+            {
+                $value = array_sum(str_split($value * 2, 1));
+            }
+            $stack += $value;
+        }
+        $stack %= 10;
+        if ($stack != 0)
+        {
+            $stack -= 10;
+        }
+        $number = implode('', array_reverse($number)) . abs($stack);
+    }
+    return $number;
+}
+
+for($i=170000;$i<250000;$i++) {
+  $j=Luhn($i);
+
+
+  if($i % 100 == 0) {
+  print 'Started new section: '.$i;
+  print "\n";
+  }
+  getData($j);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+$sorgu=mysqli_query($link, "SELECT * from `20172`");
+while($row = mysqli_fetch_array($sorgu)) {
+    $next=$row['id'];
+	print "Working on: $next\n";
+    getData($next);
+
+}
+*/
+
+print "Everything worked out just fine!";
+ mysqli_close($link);
---
+++ 219_downloader.py
@@ -0,0 +1,78 @@
+import requests
+import re
+import os
+import platform
+
+#generators for luhn checksum
+def checksum(string):
+    digits=list(map(int, string))
+    odd_sum=sum(digits[-1::-2])
+    even_sum=sum([sum(divmod(2 * d, 10)) for d in digits[-2::-2]])
+    return (odd_sum + even_sum) % 10
+
+def generate(string):
+    cksum=checksum(string + '0')
+    return (10 - cksum) % 10
+
+def luhn(iterator):
+    sit=str(iterator)
+    id=sit+str(generate(sit))
+    return int(id)
+
+#regex search pattern
+pattern='<tr><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td>'
+
+#headers for requests
+headers={
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Referer': 'http://ma219.math.metu.edu.tr/SIS/sis.php',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+}
+
+
+if platform.system() == 'Linux':
+    os.system("clear")
+else :
+    os.system("cls")
+
+print "Welcome to MATH219 Grade Downloader!"
+
+#open the file to write
+#written file format: id,lastname,firstname,grade
+try:
+    f=open("219.csv", "a")
+except:
+    print "I cannot open 219.csv, please check permissions, OR I'LL DIE LIKE RIGHT NOW"
+    exit()
+
+def requestIt(headers,data):
+    try:
+        response=requests.post('http://ma219.math.metu.edu.tr/SIS/sis.php', headers=headers, data=data)
+        return response
+    except:
+        time.sleep(5000)
+        print "Let's sleep a bit, baba yorgun"
+        requestIt(headers,data)
+
+
+for i in xrange(170000,250000):
+    data={
+      'id': luhn(i),
+      'Submit': 'Show Info'
+    }
+    if not i % 100:
+        print "Started new section: ", i
+
+    response=requestIt(headers,data)
+
+    temp=re.findall(pattern, response.content)
+    if(len(temp) > 1):
+        temp=temp[1]
+        print temp[0], "-", temp[2], temp[1], "imported"
+        f.write(temp[0]+","+temp[1]+","+temp[2]+","+temp[10]+"\n")
+
+print "That's all folks!"
